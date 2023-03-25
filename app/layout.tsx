411cf92f6aa3fe75cd4c12c358f31549954aed63
ElmerPC <erey2790@gmail.com>
2023-03-25 16:32:27 -0500

    created login component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import SideBar from "../components/SideBar";
 import { getServerSession } from "next-auth";
 import "../styles/globals.css";
 import { authOptions } from "../pages/api/auth/[...nextauth]";
+import Login from "../components/Login";
+
+
 
 
 
@@ -16,6 +19,10 @@ export default async function RootLayout({
     <html lang="en">
       <body>
         <SessionProvider session={session}>
+          {!session ? (
+            <Login/>
+          ): (
+
         <div className="flex">
           <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-[20rem]:">
             <SideBar />
@@ -23,6 +30,7 @@ export default async function RootLayout({
           {/* ClientProvider - Notifications */}
           <div className="bg-[#343541] flex-1">{children}</div>
         </div>
+          )}
         </SessionProvider>
       </body>
     </html>
