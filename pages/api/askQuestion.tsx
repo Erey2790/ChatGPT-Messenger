de3dff30a209259b335b397befd8529d3e3f1565
ElmerPC <erey2790@gmail.com>
2023-04-22 22:47:35 -0500

    setting up firebase admin

--- pages/api/askQuestion.tsx
+++ pages/api/askQuestion.tsx
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
 import type { NextApiRequest, NextApiResponse } from 'next'
+import query from '../../lib/queryApi';
 
 type Data = {
   answer: string
@@ -23,6 +25,10 @@ export default async function handler(
 
   // chatGPT Query
   const response = await query(prompt, chatId, model)
+
+  const message: Message = {
+    text: response || "ChatGPT was unable to find an answer for that!"
+  }
 
   res.status(200).json({ name: 'John Doe' })
 }
