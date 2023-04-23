6fb20b4dd50b8864b523be39facc3da13845371a
ElmerPC <erey2790@gmail.com>
2023-04-23 11:14:19 -0500

    delete

--- pages/api/askQuestion.tsx
+++ pages/api/askQuestion.tsx
@@ -2,9 +2,11 @@
 
 import type { NextApiRequest, NextApiResponse } from 'next'
 import query from '../../lib/queryApi';
+import  admin from 'firebase-admin'
+import { adminDb } from '../../firebaseAdmin';
 
 type Data = {
-  answer: string
+  answer: string,
 }
 
 export default async function handler(
@@ -20,15 +22,30 @@ export default async function handler(
   }
 
   if (!chatId) {
-    res.status(200).json({ answer: "Please provide a valid chat ID!" })
+    res.status(400).json({ answer: "Please provide a valid chat ID!" })
+    return;
   }
 
   // chatGPT Query
   const response = await query(prompt, chatId, model)
 
   const message: Message = {
-    text: response || "ChatGPT was unable to find an answer for that!"
-  }
+    text: response || "ChatGPT was unable to find an answer for that!",
+    createdAt: admin.firestore.Timestamp.now(),
+    user: {
+      _id: 'ChatGPT',
+      name: 'ChatGPT',
+      avatar: "https://links.papareact.com/89k"
+    }
+  };
 
-  res.status(200).json({ name: 'John Doe' })
+  await adminDb
+  .collection('users')
+  .doc(session?.user?.email)
+  .collection("chats")
+  .doc(chatId)
+  .collection("messages")
+  .add(message)
+
+  res.status(200).json({ answer: message.text })
 }
