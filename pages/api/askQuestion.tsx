5b8377835f81ee6ba3e63dc2ee3f00e572ff97c1
ElmerPC <erey2790@gmail.com>
2023-04-22 20:51:13 -0500

    creating the link for api

--- pages/api/askQuestion.tsx
+++ pages/api/askQuestion.tsx
@@ -2,12 +2,27 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  answer: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
-) {
+) { 
+
+  const { prompt, chatId, model, session } = req.body;
+
+  if (!prompt) {
+    res.status(400).json({ answer: "Please provide a prompt!" });
+    return;
+  }
+
+  if (!chatId) {
+    res.status(200).json({ answer: "Please provide a valid chat ID!" })
+  }
+
+  // chatGPT Query
+  const response = await query(prompt, chatId, model)
+
   res.status(200).json({ name: 'John Doe' })
 }
