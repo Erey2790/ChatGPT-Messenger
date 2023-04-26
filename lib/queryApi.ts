e07bd1a0d6b17122b5c0f1c51485298e1d201185
ElmerPC <erey2790@gmail.com>
2023-04-26 15:25:15 -0500

    openai key updated

--- lib/queryApi.ts
+++ lib/queryApi.ts
@@ -9,7 +9,7 @@ const query = async (prompt: string, chatId: string, model: string) => {
         max_tokens: 1000,
         frequency_penalty: 0,
         presence_penalty: 0,
-    }).then(res => res.data.choices[0].text)
+    }).then((res) => res.data.choices[0].text)
       .catch(
         (err) =>
         `ChatGPT was unable to find an answer for that! (Error: ${err.message})`
